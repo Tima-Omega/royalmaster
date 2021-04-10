94a7f818bde6f3d3990c0ad809c632db0e6065b7
Tima-Omega <tima_omega@outlook.com>
2021-04-10 16:37:30 +0300

    added burger menu

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-$(".contact__input").mask("(999) 999-99-99");
+$(document).ready(function(){
+    $(".contact__input").mask("(999) 999-99-99");
+    $(".burger__btn").click(function(){
+        $(".burger__list").slideToggle();
+    })
+})
