c6798c42f87421b6823d368fa2998c2fa7d9fd76
Tima-Omega <tima_omega@outlook.com>
2021-05-12 17:43:47 +0300

    added 2 popup

--- js/app.js
+++ js/app.js
@@ -2,20 +2,32 @@ $(document).ready(function(){
     $(".contact__input").mask("(999) 999-99-99");
     $(".burger__btn").click(function(){
         $(".burger__list").slideToggle();
-    })
-})
+    });
+});
 $('.overlay').click(function () {
     $(this).fadeOut();
     $('.modal').fadeOut();
-})
+});
 $('.wedo__btn').click(function() {
     $('.overlay').fadeIn();
-    $('.modal').fadeIn();
+    $('.modal__wedo').fadeIn();
+});
+$('.order').click(function() {
+    $('.overlay').fadeIn();
+    $('.modal--order-first').fadeIn();
+});
+$('.order__mail').click(function() {
+    $('.overlay').fadeIn();
+    $('.modal--order-second').fadeIn();
+});
+$('.done').click(function() {
+    $('.overlay').fadeIn();
+    $('.modal--order-done').fadeIn();
 });
 $('.modal__close').click(function () {
     $(this).closest('.modal').fadeOut();
     $('.overlay').fadeOut();
-})
+});
 
 $(document).keydown(function(e) {
     if (e.keyCode === 27) {
